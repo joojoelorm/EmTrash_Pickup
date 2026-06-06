29505f81a63d3603018d37159386f8c8fea34948
joojoelorm <samuelfamenu@gmail.com>
2026-06-06 01:30:55 +0000

    Update geo-config.js

--- js/geo-config.js
+++ js/geo-config.js
@@ -1,3 +1,5 @@
+// Optional: add a Geoapify API key for better landmark/address search.
+// Sign up at https://www.geoapify.com/ and paste the key below.
 export const GEO_CONFIG = {
   geoapifyKey: "310025c89cdd42988e5dc698f27010f2",
 };
